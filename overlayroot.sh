64877f9dec4b0e364d36ddb99700c71ca50c1e7a
Michael F. Schönitzer <michael@schoenitzer.de>
2015-06-14 14:14:15 +0200

    kosmetische Änderungen

--- overlayroot.sh
+++ overlayroot.sh
@@ -6,37 +6,38 @@ set -e # exit on error
 pdir=/tmp
 rootdir=${pdir}/newroot
 cdir=${pdir}/overlay
-csize=64 # size of container in MByte
+csize=256 # size of container in MByte
 upperdir=${cdir}/upperdir
 workdir=${cdir}/work
 
-if [ $EUID -ne 0 ]; then
-    echo "Error: This script must be run as root."
-    exit 1
-fi
+
+err() {
+   echo $*
+   exit
+}
 
 umount_all() {
-  if [[ "$rootdir" = "" ]] ; then
-    echo "Error: Can't dismount."; return
-  fi
-  cd /
-  cleaned=0
-  echo -n "Releasing mounts within container: " 
-  # unmount recursively until all mounts in $rootdir are gone
-  while [[ $cleaned -eq 0 ]]; do
-    cleaned=1
-	for i in $(cat /proc/mounts | awk '{print $2}' | grep "^$rootdir" | sort -r); do
-       umount 2>/dev/null "${i/\\040/ }" && echo "umounted ${i/\\040/ } succesfully" || cleaned=0
-    done
-  done
-    echo " done."
+   if [[ "$rootdir" = "" ]] ; then
+      echo "Error: Can't dismount."; return
+   fi
+   cd /
+   cleaned=0
+   echo -n "Releasing mounts within container: " 
+   # unmount recursively until all mounts in $rootdir are gone
+   while [[ $cleaned -eq 0 ]]; do
+      cleaned=1
+      for i in $(cat /proc/mounts | awk '{print $2}' | grep "^$rootdir" | sort -r); do
+         umount 2>/dev/null "${i/\\040/ }" && echo "umounted ${i/\\040/ } succesfully" || cleaned=0
+      done
+   done
+      echo " done."
 
-  umount $cdir 2> /dev/null || :
+   umount $cdir 2>/dev/null || :
 }
-err() {
-  echo $*
-  exit
-}
+
+if [ $EUID -ne 0 ]; then
+      err "Error: This script must be run as root."
+fi
 
 # make sure $rootdir is an empty dir
 test -d $rootdir || mkdir -p $rootdir
@@ -45,27 +46,27 @@ test -d $rootdir || mkdir -p $rootdir
 # The file for the container can be given in four different ways:
 # * absolute path to file (exp: /home/bar/foo)
 # * relative path (exp: data/foo)
-# * -n and pure filename (exp: -n foo) - IN THIS CASE IT WILL BE PUT IN THE URS-HOME-DIR
+# * -n and pure filename (exp: -n foo) - in this case it will be put in the urs-home-dir
 # * none - In this case we use ~/overlay
 if [[ $# -eq 0 ]]; then
-  cfile=${HOME}/overlay
+   cfile=${HOME}/overlay
 elif [[ $# -eq 1 ]]; then
-  cfile=$(readlink -m $1)
+   cfile=$(readlink -m $1)
 elif [[ $# -eq 2 ]]; then
-  if [[ "$1" = "-n" ]]; then
-    cfile=${HOME}/$(basename $2)
-  else
-    err "Wrong usage"
-  fi
+   if [[ "$1" = "-n" ]]; then
+      cfile=${HOME}/$(basename $2)
+   else
+      err "Wrong usage"
+   fi
 else
-  err "Wrong usage"
+   err "Wrong usage"
 fi
 
 # Test if kernel uses grsecurity with chroot_deny_unix
 # if this is the case disable it, otherwise sokets won't work
 # this would prevent you from running graphical programs
 if [[ $(sysctl kernel.grsecurity.chroot_deny_unix 2>/dev/null | tail -c2) -eq 1 ]] ; then
-	sysctl kernel.grsecurity.chroot_deny_unix=0
+   sysctl kernel.grsecurity.chroot_deny_unix=0
 fi
 
 # Mount kernelmodule of overlayfs
@@ -75,8 +76,8 @@ modprobe overlay
 # create otherwise
 echo $cfile 
 if [[ ! -f $cfile ]]; then
-  dd if=/dev/zero of=$cfile bs=1M count=$csize status=none
-  mkfs.ext4 -qF $cfile
+   dd if=/dev/zero of=$cfile bs=1M count=$csize status=none
+   mkfs.ext4 -qF $cfile
 fi
 
 # if anything fails make sure all mounts are cleaned up
@@ -104,14 +105,14 @@ a=()
 for i in $(cat /proc/mounts | awk '{print $2}' | grep "^/" | \
            egrep -v '^/(run|sys|dev|proc|$)' |\
            awk '{ print length, $0 }' | sort -n | cut -d" " -f2-); do
-    for j in ${a[*]}; do
+   for j in ${a[*]}; do
       if echo "$i" | grep -q "^$j"; then
-        continue 2
+         continue 2
       fi
-    done
-    echo mount --rbind "$i" ".$i"
-    mount --rbind "$i" ".$i"
-    a[${#a[*]}]="$i"
+   done
+   echo mount --rbind "$i" ".$i"
+   mount --rbind "$i" ".$i"
+   a[${#a[*]}]="$i"
 done
 
 # Allow connection to X-Server from within chroot
@@ -119,7 +120,7 @@ xhost +local: || :
 
 # Put bindmounts in slave mode
 for i in $(cat /proc/mounts | awk '{print $2}' | grep $rootdir); do
-	mount --make-slave "${i/\\040/ }"
+   mount --make-slave "${i/\\040/ }"
 done
 
 # Start chroot
